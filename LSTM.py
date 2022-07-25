e6fc9f047dce4c4b346fb6f325dc04b84808fb1c
Anjali Agarwal <anjaliagrl8@gmail.com>
2022-07-25 19:35:28 +0530

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -13,47 +13,58 @@ from keras_preprocessing.sequence import pad_sequences
 import nltk
 nltk.download('punkt')
 
-cleaned = re.sub(r'\W+', ' ', doc).lower()
-tokens = word_tokenize(cleaned)
-train_len = 3+1
-text_sequences = []
-for i in range(train_len,len(tokens)):
-    seq = tokens[i-train_len:i]
-    text_sequences.append(seq)
-sequences = {}
-count = 1
-for i in range(len(tokens)):
-    if tokens[i] not in sequences:
-        sequences[tokens[i]] = count
-        count += 1
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(text_sequences)
-sequences = tokenizer.texts_to_sequences(text_sequences) 
-
-#Collecting some information   
-vocabulary_size = len(tokenizer.word_counts)+1
-
-n_sequences = np.empty([len(sequences),train_len], dtype='int32')
-for i in range(len(sequences)):
-    n_sequences[i] = sequences[i]
+def preprocess(doc):
+    cleaned = re.sub(r'\W+', ' ', doc).lower()
+    tokens = word_tokenize(cleaned)
+    train_len = 3+1
+    text_sequences = []
+    for i in range(train_len,len(tokens)):
+        seq = tokens[i-train_len:i]
+        text_sequences.append(seq)
+    sequences = {}
+    count = 1
+    for i in range(len(tokens)):
+        if tokens[i] not in sequences:
+            sequences[tokens[i]] = count
+            count += 1
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(text_sequences)
+    sequences = tokenizer.texts_to_sequences(text_sequences) 
     
-train_inputs = n_sequences[:,:-1]
-train_targets = n_sequences[:,-1]
-train_targets = to_categorical(train_targets, num_classes=vocabulary_size)
-seq_len = train_inputs.shape[1]
+    #Collecting some information   
+    vocabulary_size = len(tokenizer.word_counts)+1
+    
+    n_sequences = np.empty([len(sequences),train_len], dtype='int32')
+    for i in range(len(sequences)):
+        n_sequences[i] = sequences[i]
         
-model = Sequential()
-model.add(Embedding(vocabulary_size, seq_len, input_length=seq_len))
-model.add(LSTM(50,return_sequences=True))
-model.add(LSTM(50))
-model.add(Dense(50,activation='relu'))
-model.add(Dense(vocabulary_size, activation='softmax'))
-print(model.summary())
+    train_inputs = n_sequences[:,:-1]
+    train_targets = n_sequences[:,-1]
+    train_targets = to_categorical(train_targets, num_classes=vocabulary_size)
+    seq_len = train_inputs.shape[1]
+    
+    return vocabulary_size, seq_len, train_inputs, train_targets, tokenizer
+
+def model(vocab_size, seq_len):
+    model = Sequential()
+    model.add(Embedding(vocab_size, seq_len, input_length=seq_len))
+    model.add(LSTM(50,return_sequences=True))
+    model.add(LSTM(50))
+    model.add(Dense(50,activation='relu'))
+    model.add(Dense(vocab_size, activation='softmax'))
+    print(model.summary())
+    
+    return model
+    
+    
+vocab_size, seq_len, train_inputs, train_targets, tokenizer = preprocess(training_doc3)
+model = model(vocab_size, seq_len)
 # compile network
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(train_inputs,train_targets,epochs=500,verbose=1)
-model.save("mymodel.h5")
-    
+#model.save("mymodel.h5")
+   
+# Testing the model
 input_text = input().strip().lower()
 encoded_text = tokenizer.texts_to_sequences([input_text])[0]
 pad_encoded = pad_sequences([encoded_text], maxlen=seq_len, truncating='pre')
